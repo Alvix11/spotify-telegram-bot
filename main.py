bf28f9baf7c531d77df14d900b3b984e80facbb5
Alvix.exe <alvix.wick@gmail.com>
2025-03-02 21:18:52 -0400

    Update Spanish comments to English.

--- main.py
+++ main.py
@@ -1,24 +1,22 @@
-# main.py
-
 from telegram.ext import Application, CommandHandler, MessageHandler, filters
 from config import TELEGRAM_TOKEN
 from database import init_db, create_database
 from bot_handlers import start, manejar_enlace, manejar_errores
 
 def main():
-    # Inicializar la base de datos
+    # Start database.
     create_database()
     init_db()
 
-    # Crear la aplicación del bot
+    # Create bot application
     application = Application.builder().token(TELEGRAM_TOKEN).build()
 
-    # Registrar los manejadores
+    # Register the handlers
     application.add_handler(CommandHandler("start", start))
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, manejar_enlace))
     application.add_error_handler(manejar_errores)
 
-    # Iniciar el polling
+    # Start polling
     application.run_polling()
 
 if __name__ == '__main__':
